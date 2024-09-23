ab98ab620190b2e6d3cf087f54e0cf95c2547092
Sharadhi Hegde <hegdesharadhi@gmail.com>
2024-09-23 12:04:17 +0530

    Practice on react-query

--- src/Items.js
+++ src/Items.js
@@ -1,11 +1,27 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import SingleItem from './SingleItem'
+import { useFetchTasks } from './reactQueryCustomHooks'
+
+const Items = () => {
+  const { isPending, isError, data } = useFetchTasks()
+
+  if (isPending) {
+    return <p style={{ marginTop: '1rem' }}>Loading...</p>
+  }
+
+  if (isError) {
+    return <p style={{ marginTop: '1rem' }}>There was an error...</p>
+  }
+
+  // if (error) {
+  //   return <p style={{ marginTop: '1rem' }}>{error.message}</p>
+  // }
+
   return (
-    <div className='items'>
-      {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
+    <div className="items">
+      {data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />
       })}
     </div>
-  );
-};
-export default Items;
+  )
+}
+export default Items
